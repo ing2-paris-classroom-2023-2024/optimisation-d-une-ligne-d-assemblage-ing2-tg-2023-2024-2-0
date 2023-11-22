5d50c6780e42647606225deaa70377d5b89c87fc
Maxlepalmier <maxime.delplace@edu.ece.fr>
2023-11-22 14:34:20 +0100

    Ajout programme Maxime

--- header.h
+++ header.h
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#define NUM_OPERATIONS 35
+#define MAX_CONSTRAINTS 100
+
 /////////////////////////////////////////////Programme Gabriel 1.2//////////////////////////////////////////////////////
 typedef struct Noeud {
     int sommet;
@@ -23,6 +26,23 @@ void ajouterArete(Graphe* graphe, int src, int dest);
 void PredecesseursRecursif(Graphe* graphe, int sommet, int sommetInitial, int visite[]);
 void Predecesseurs(Graphe* graphe);
 void algorithemPertsanspoids();
+
+
+////////////////////////////////Programme Maxime//////////////////////////////
+struct AssignmentProblem {
+    int stations[NUM_OPERATIONS];
+    struct ExclusionConstraint {
+        int operation1;
+        int operation2;
+    } exclusionConstraints[MAX_CONSTRAINTS];
+    int numConstraints;
+};
+
+void initialisation_stations(int *stations);
+int nbr_stations(const int *stations);
+int affectation_valide(const int *stations, const struct ExclusionConstraint *constraints, int numConstraints, int currentOperation, int currentStation);
+int station_min_conflit(const int *stations, const struct ExclusionConstraint *constraints, int numConstraints, int currentOperation);
+void probleme_affectation(struct AssignmentProblem *problem);
 
 
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_2_0_HEADER_H
--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include "header.h"
 
 int main(){
+
+////////////////Sous Programme Gabriel/////////////////
     algorithemPertsanspoids();
+
+//////////////////Sous Programme Maxime/////////
+    struct AssignmentProblem problem;
+    probleme_affectation(&problem);
     return 0;
 }
