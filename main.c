4ac118dae964ec33af5b0da55cd0c26f7316d346
Julien <julienbitschenauxdurand@gmail.com>
2023-11-28 08:45:33 +0100

    Avancement Algo Temps de Cycle 4.

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include "header.h"
 
 int main(){
+
+    ////////////////Sous Programme Julien/////////////////
+    int nombreOperations = 0;
+    Operation* operations = lireOperations(&nombreOperations);
+
+    // Triez les opérations par ordre croissant en fonction de leur temps d'exécution
+    trierOperations(operations, nombreOperations);
+
+    // Créer des tâches à partir des opérations triées
+    int nombreTaches = 0;
+    Tache* taches = creerTaches(operations, nombreOperations, &nombreTaches);
+
+    // Afficher les tâches créées
+    afficherTaches(taches, nombreTaches);
+
+    // Libérer la mémoire allouée
+    free(operations);
+    for (int i = 0; i < nombreTaches; ++i) {
+        free(taches[i].operations);
+    }
+    free(taches);
+
 ////////////////Sous Programme Gabriel/////////////////
     algorithemPertsanspoids();
 
